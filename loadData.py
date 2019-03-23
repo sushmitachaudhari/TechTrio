60e4edab95e7d805b8d65ec9e9aa5e232c4023de
Namrata Bilurkar <namratab94@gmail.com>
2019-03-23 15:11:55 -0400

    Load data into a pickle

--- loadData.py
+++ loadData.py
@@ -1,13 +1,31 @@
 import csv
+import pickle
+import os
+from datetime import datetime
 
 DATA = "crimes_data.csv"
 
 reader = csv.DictReader(open(DATA, 'r'))
+PICKLE_FILE = "dataDict.p"
 
-dataDict = dict()
 
-instance_counter = 0
-for line in reader:
-	instance_counter += 1
-	dataDict[instance_counter] = line
+def loadFile():
+	dataDict = dict()
 
+	instance_counter = 0
+	for line in reader:
+		instance_counter += 1
+		dataDict[instance_counter] = line
+
+	# delete only if file exists
+	if os.path.exists(PICKLE_FILE):
+	    os.remove(PICKLE_FILE)
+	else:
+	    pickle.dump(dataDict, open(PICKLE_FILE, "wb"))
+
+
+if __name__ == '__main__':
+	start_time = datetime.now()
+	loadFile()
+	end_time = datetime.now()
+	print("Time taken to load the pickle: " + str(end_time - start_time))
